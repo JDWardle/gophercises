e3c9381e194f5ddd499e3c7713f187389f7d9fe8
Jesse Wardle <wardlejesse@gmail.com>
2018-06-01 11:51:15 -0600

    Quiz part 2 and bonus.

--- quiz/main.go
+++ quiz/main.go
@@ -4,16 +4,23 @@ import (
 	"encoding/csv"
 	"flag"
 	"fmt"
+	"math/rand"
 	"os"
 	"path/filepath"
+	"strings"
+	"time"
 )
 
 var (
 	problemsFile string
+	timeLimit    time.Duration
+	randomize    bool
 )
 
 func main() {
 	flag.StringVar(&problemsFile, "problems", "problems.csv", "define the path of a CSV list of in the format of `question,answer`")
+	flag.DurationVar(&timeLimit, "timer", time.Duration(30*time.Second), "sets the time limit of the quiz")
+	flag.BoolVar(&randomize, "randomize", false, "sets the quiz to be randomized each run")
 	flag.Parse()
 
 	p, err := filepath.Abs(problemsFile)
@@ -32,15 +39,41 @@ func main() {
 		panic(fmt.Errorf("failed to parse CSV file `%s`", problemsFile))
 	}
 
+	fmt.Printf("Press enter to start quiz...")
+	fmt.Scanln()
+
+	if randomize {
+		rand.Shuffle(len(problems), func(i, j int) {
+			problems[i], problems[j] = problems[j], problems[i]
+		})
+	}
+
 	correctAnswers := 0
+	timer := time.After(timeLimit)
+	doneChan := make(chan struct{})
 
-	for _, problem := range problems {
-		fmt.Printf("%s: ", problem[0])
-		var answer string
-		fmt.Scanln(&answer)
+	go func() {
+		for _, problem := range problems {
+			fmt.Printf("%s: ", problem[0])
+			var answer string
+			fmt.Scanln(&answer)
 
-		if answer == problem[1] {
-			correctAnswers++
+			if answer == strings.TrimSpace(problem[1]) {
+				correctAnswers++
+			}
+		}
+		<-doneChan
+	}()
+
+questionAnswerLoop:
+	for {
+		select {
+		case <-timer:
+			fmt.Println("\nout of time!")
+			break questionAnswerLoop
+		case <-doneChan:
+			fmt.Println("done!")
+			break questionAnswerLoop
 		}
 	}
 
